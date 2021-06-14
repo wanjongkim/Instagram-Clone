b316c4e2608de80a5c08020facfd4d90c056d9e6
Wan Jong Kim <wanjongkim1@gmail.com>
2021-06-13 22:11:16 -0400

    Added Like button

--- front-end/client/src/components/screens/Home.js
+++ front-end/client/src/components/screens/Home.js
@@ -1,7 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
+import {userContext} from '../../App';
 
 const Home = () => {
     const [data, setData] = useState([]);
+    const {state, dispatch} = useContext(userContext);
     useEffect(()=> {
         fetch('/allposts', {
             headers: {
@@ -9,9 +11,54 @@ const Home = () => {
             }
         }).then(res=>res.json())
         .then(result=>{
+            console.log(result);
             setData(result)
         })
     }, []);
+    const likePost = (id) => {
+        fetch('/like', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+            const newData = data.map(item=> {
+                if(item._id == result._id) {
+                    return result;
+                }
+                return item;
+            })
+            setData(newData);
+        })
+    }
+    const unlikePost = (id) => {
+        fetch('/unlike', {
+            method: 'put',
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + localStorage.getItem("jwt")
+            },
+            body: JSON.stringify({
+                postId: id
+            })
+        }).then(res=>res.json())
+        .then(result=> {
+            const newData = data.map(item=> {
+                if(item._id == result._id) {
+                    return result;
+                }
+                else {
+                    return item;
+                }
+            })
+            setData(newData);
+        })
+    }
     return (
         <div className="home">
             {
@@ -24,6 +71,9 @@ const Home = () => {
                         </div>
                         <div className="card-content">
                             <i className="material-icons" style={{color: "red"}}>favorite</i>
+                            {item.likes.includes(state._id) ? 
+                            <i className="material-icons" onClick={()=> {unlikePost(item._id)}}>thumb_down</i> : <i className="material-icons" onClick={()=> {likePost(item._id)}}>thumb_up</i>}
+                            <h6>{item.likes.length} likes</h6>
                             <h6>{item.title}</h6>
                             <p>{item.body}</p>
                             <input type="text" placeholder="add a comment"/>
--- routes/post.js
+++ routes/post.js
@@ -7,7 +7,7 @@ const Post = mongoose.model("Post");
 
 router.get('/mypost', requireLogin, (req, res) => {
     Post.find({postedBy: req.user._id})
-    .populate("PostedBy", "_id name")
+    .populate("postedBy", "_id name")
     .then(mypost=> {
         res.json({mypost})
     })
@@ -71,6 +71,26 @@ router.put('/unlike', requireLogin, (req, res) => {
         }
         else {
             res.json(result);
+        }
+    })
+})
+
+router.put('/comment',requireLogin,(req,res)=>{
+    const comment = {
+        text:req.body.text,
+        postedBy:req.user._id
+    }
+    Post.findByIdAndUpdate(req.body.postId,{
+        $push:{comments:comment}
+    },{
+        new:true
+    })
+    .populate("comments.postedBy","_id name")
+    .exec((err,result)=>{
+        if(err){
+            return res.status(422).json({error:err})
+        }else{
+            res.json(result)
         }
     })
 })
